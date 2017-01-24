2d7efefc5a48f4b7e3e6c17882f8b620bddd3881
17liamnaddell <liamnprg@gmail.com>
2017-01-23 19:45:49 -0500

    lul

---
+++ guessmynumber/guessmynumber/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  guessmynumber
+//
+//  Created by Liam Naddell on 1/23/17.
+//  Copyright © 2017 Liam Naddell. All rights reserved.
+//
+
+import Foundation
+
+print("wat u naem?")
+
+if let yourName = readLine() {
+    print("hai", yourName)
+}
